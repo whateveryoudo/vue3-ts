f4aceaf49d7fb288cde969017ebfd97a759301fd
youkaixing <1795261900@qq.com>
2021-01-05 18:56:50 +0800

    初始化目录结构

---
+++ examples/index.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Author: ykx
+ * @Date: 2021-01-05 18:56:15
+ * @LastEditTime: 2021-01-05 18:56:27
+ * @LastEditors: your name
+ * @Description: 
+ * @FilePath: \vue3-ts\examples\index.vue
+-->
+<template>
+  <div>vue3</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+};
+</script>
+<style scoped>
+</style>
+
